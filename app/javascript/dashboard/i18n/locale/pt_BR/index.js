9e9834425f0637dcee727495da7ced6e703a68b0
Fernando Morais <dev.fernandomorais@gmail.com>
2026-03-24 18:06:00 -0300

    feat: Enhance localization for Portuguese by adding new keys for various components, including agent bots, attributes management, automation, canned responses, and more. Introduce search placeholders, counts, and no results messages to improve user experience across the dashboard.

--- app/javascript/dashboard/i18n/locale/pt_BR/index.js
+++ app/javascript/dashboard/i18n/locale/pt_BR/index.js
@@ -8,6 +8,7 @@ import bulkActions from './bulkActions.json';
 import campaign from './campaign.json';
 import cannedMgmt from './cannedMgmt.json';
 import chatlist from './chatlist.json';
+import companies from './companies.json';
 import components from './components.json';
 import contact from './contact.json';
 import contactFilters from './contactFilters.json';
@@ -33,8 +34,13 @@ import setNewPassword from './setNewPassword.json';
 import settings from './settings.json';
 import signup from './signup.json';
 import sla from './sla.json';
+import snooze from './snooze.json';
 import teamsSettings from './teamsSettings.json';
+import tickets from './tickets.json';
 import whatsappTemplates from './whatsappTemplates.json';
+import contentTemplates from './contentTemplates.json';
+import mfa from './mfa.json';
+import yearInReview from './yearInReview.json';
 
 export default {
   ...advancedFilters,
@@ -47,6 +53,7 @@ export default {
   ...campaign,
   ...cannedMgmt,
   ...chatlist,
+  ...companies,
   ...components,
   ...contact,
   ...contactFilters,
@@ -72,6 +79,11 @@ export default {
   ...settings,
   ...signup,
   ...sla,
+  ...snooze,
   ...teamsSettings,
+  ...tickets,
   ...whatsappTemplates,
+  ...contentTemplates,
+  ...mfa,
+  ...yearInReview,
 };
